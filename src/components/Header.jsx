c099610cc608a9e0aa7ef4d0e5cf07952a837532
ritik namdev <ritiknamdev5178@gmail.com>
2024-07-23 17:49:19 +0530

    Notes-Keeper Project complete

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import NotesSharpIcon from '@mui/icons-material/NotesSharp';
+function Header() {
+  return (
+    <header>
+
+      <h1>    <NotesSharpIcon style={{fontSize:"larger"}} />Notes Keeper</h1>
+    </header>
+  );
+}
+
+export default Header;
